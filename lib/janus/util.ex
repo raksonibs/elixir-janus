b12581de83cb329a15307d871352f2e6ee87f6d0
Oskar Niburski <oskarniburski@gmail.com>
2019-08-09 18:46:37 -0700

    update(janus): Attempt util rewrite

--- lib/janus/util.ex
+++ lib/janus/util.ex
@@ -76,6 +76,49 @@ defmodule Janus.Util do
     end
   end
 
+
+  def get(url) do
+    Logger.debug("GET #{url}")
+    case HTTPoison.get(url, [], recv_timeout: :infinity) do
+      {:ok, %HTTPoison.Response{body: body}} ->
+        case Poison.decode(body, keys: :atoms) do
+          {:ok, %{janus: "error", error: error}} ->
+            Logger.error(error.reason)
+            {:error, error.reason}
+          {:error, error} ->
+            Logger.error(error)
+            {:error, error}
+          {:ok, v} ->
+            Logger.debug(inspect(v))
+            {:ok, v}
+        end
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        Logger.error(reason)
+        {:error, reason}
+    end
+  end
+
+  def post(url, body) do
+    Logger.debug("POST #{url} (#{inspect(body)})")
+    case HTTPoison.post(url, Poison.encode!(add_transaction_id(body))) do
+      {:ok, %HTTPoison.Response{body: body}} ->
+        case Poison.decode(body, keys: :atoms) do
+          {:ok, %{janus: "error", error: error}} ->
+            Logger.error(error.reason)
+            {:error, error.reason}
+          {:error, error} ->
+            Logger.error(error)
+            {:error, error}
+          {:ok, v} ->
+            Logger.debug(inspect(v))
+            {:ok, v}
+        end
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        Logger.error(reason)
+        {:error, reason}
+    end
+  end
+
   def maybe_add_key(map, _key, nil), do: map
   def maybe_add_key(map, key, value), do: Map.put(map, key, value)
 
