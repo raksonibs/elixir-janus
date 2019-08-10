14550711dd0c8b1460f8da39cd51bbb60c03db22
Oskar Niburski <oskarniburski@gmail.com>
2019-08-10 16:02:07 -0700

    fix(janus): Fix url binding

--- lib/janus/session.ex
+++ lib/janus/session.ex
@@ -12,8 +12,6 @@ defmodule Janus.Session do
     :id,
     :base_url,
     :event_manager,
-    :cookie,
-    :room_name,
     handles: %{}
   ]
 
@@ -24,27 +22,16 @@ defmodule Janus.Session do
   Use this function if you must perform additional configuration steps before the session is started.
   """
 
-  def init(url, room_name, initial_cookie) do
-    case post(url, initial_cookie, %{janus: :create}) do
-      {:ok, body, new_cookie} ->
-        if new_cookie != nil do
-          ConCache.put(:room_cache, room_name, new_cookie)
-        else
-          # If new cookie is nil .. take it from cache
-          new_cookie = ConCache.get(:room_cache, room_name)
-
-          IO.inspect("init::cookie from cache.. should never be nil": new_cookie)
-        end
-
+  def init(url) do
+    case post(url, %{janus: :create}) do
+      {:ok, body} ->
         id = body.data.id
         {:ok, event_manager} = GenEvent.start_link()
 
         session = %Janus.Session{
           id: id,
           base_url: "#{url}/#{id}",
-          event_manager: event_manager,
-          cookie: new_cookie,
-          room_name: room_name
+          event_manager: event_manager
         }
 
         Agent.start(fn -> session end)
@@ -58,11 +45,8 @@ defmodule Janus.Session do
 
   def start(pid) when is_pid(pid), do: poll(pid)
 
-  def start(url, room_name) do
-    # Get cookie for this room name if already exists
-    cookie = ConCache.get(:room_cache, room_name)
-
-    case init(url, room_name, cookie) do
+  def start(url) do
+    case init(url) do
       {:ok, pid} ->
         start(pid)
         {:ok, pid}
@@ -84,89 +68,17 @@ defmodule Janus.Session do
   def attach_plugin(pid, id) do
     base_url = Agent.get(pid, & &1.base_url)
 
-    # get cookie
-    main_cookie = Agent.get(pid, & &1.cookie)
-    room_name = Agent.get(pid, & &1.room_name)
-
-    IO.inspect("attach_plugin:: main cookie - should never be nil": main_cookie)
-
     v =
-      case post(base_url, main_cookie, %{janus: :attach, plugin: id}) do
-        {:ok, body, _cookie} ->
+      case post(base_url, %{janus: :attach, plugin: id}) do
+        {:ok, body} ->
           id = body.data.id
           {:ok, event_manager} = GenEvent.start_link()
 
-          plugin_base_url = "#{base_url}/#{id}"
-
-          room_number = ConCache.get(:room_number_cache, room_name)
-
-          room_number = if room_number != nil do
-            room_number =
-              case Janus.Plugin.check_room_exists(
-                     plugin_base_url,
-                     %{
-                       request: "exists",
-                       room: room_number
-                     },
-                     main_cookie
-                   ) do
-                {:ok, %{janus: "success", plugindata: %{data: %{exists: true}}}, _cookie} ->
-                  room_number
-
-                {:ok, %{janus: "success", plugindata: %{data: %{exists: false}}}, _cookie} ->
-                  nil
-
-                _ ->
-                  Logger.error("Invalid request")
-                  nil
-              end
-          else
-            nil
-          end
-
-          plugin = if room_number == nil do
-            IO.puts("Creating new Janus room")
-
-            {:ok, response, _cookie} =
-              Janus.Plugin.create(
-                plugin_base_url,
-                %{
-                  request: "create",
-                  description: room_name,
-                  bitrate: 2_000_000,
-                  videocodec: "h264",
-                  publishers: 20,
-                  permanent: true,
-                  audiolevel_ext: true,
-                  audiolevel_event: true,
-                  audio_active_packets: 100,
-                  audio_level_average: 120
-                },
-                main_cookie
-              )
-
-            room_number = response.plugindata.data.room
-
-            ConCache.put(:room_number_cache, room_name, room_number)
-
-            %Janus.Plugin{
-              id: id,
-              base_url: plugin_base_url,
-              event_manager: event_manager,
-              cookie: main_cookie,
-              room_number: room_number
-            }
-          else
-            IO.puts("Using existing Janus room")
-
-            %Janus.Plugin{
-              id: id,
-              base_url: plugin_base_url,
-              event_manager: event_manager,
-              cookie: main_cookie,
-              room_number: room_number
-            }
-          end
+          plugin = %Janus.Plugin{
+            id: id,
+            base_url: "#{base_url}/#{id}",
+            event_manager: event_manager
+          }
 
           {:ok, plugin_pid} = Agent.start(fn -> plugin end)
 
@@ -188,14 +100,10 @@ defmodule Janus.Session do
 
   def destroy(pid) do
     base_url = Agent.get(pid, & &1.base_url)
-    cookie = Agent.get(pid, & &1.cookie)
-
-    IO.inspect("destroy..  cookie.. should never be nil ": cookie)
-
     plugin_pids = Agent.get(pid, & &1.handles) |> Map.values()
     Enum.each(plugin_pids, &Janus.Plugin.detach(&1))
     Agent.stop(pid)
-    post(base_url, cookie, %{janus: :destroy})
+    post(base_url, %{janus: :destroy})
   end
 
   @doc "See `GenEvent.add_handler/3`."
@@ -241,8 +149,8 @@ defmodule Janus.Session do
     session = Agent.get(pid, & &1)
 
     spawn(fn ->
-      case get(session.base_url, session.cookie) do
-        {:ok, data, cookie} ->
+      case get(session.base_url) do
+        {:ok, data} ->
           event_manager = session.event_manager
 
           case data do
