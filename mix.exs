b823e878120f73429e652cab969346e64b60d037
Oskar Niburski <oskarniburski@gmail.com>
2019-08-09 18:47:06 -0700

    bump(bump): bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Janus.Mixfile do
   def project do
     [
       app: :janus,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.3",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
